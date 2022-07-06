3f91893bca8811b666c9284ed4304347e4614b6e
Ibrahem ahmad <53955363+ibrahemahmad@users.noreply.github.com>
2022-07-06 10:12:12 +0300

    more page

---
+++ src/view/aboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AboutPage(props) {
+    const {id}=props;
+  return (
+    <div>
+        
+        AboutPage {id||"n"}
+    
+    </div>
+  )
+}
