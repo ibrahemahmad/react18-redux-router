0079bf89051fdae85aa28bb930925a2a677c5e0b
Ibrahem ahmad <53955363+ibrahemahmad@users.noreply.github.com>
2022-09-20 11:23:03 +0300

    get todo by simple way

---
+++ src/view/typeTodo/TodoSimple.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { getTodo } from '../../actions/todoActions'
+import { TodoCards } from '../../components/TodoCards'
+
+export default function TodoSimple() {
+  const isLoad = useRef(false)
+  const [listTodo, setListTodo] = useState([])
+  useEffect(() => {
+    if (!isLoad.current) {
+      isLoad.current = true
+      getTodo('todos')
+        .then((data) => {
+          setListTodo(data)
+        })
+        .catch((err) => {
+          isLoad.current = false
+        })
+    }
+  }, [])
+  return listTodo?.length > 0 ? (
+    listTodo
+      .slice(0, 20)
+      .reverse()
+      ?.map((eTodo) => (
+        <TodoCards
+          title={eTodo?.id}
+          discription={eTodo.title}
+          isCompleted={eTodo?.completed}
+        />
+      ))
+  ) : (
+    <></>
+  )
+}
