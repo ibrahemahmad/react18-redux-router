29c1302576515cdd7e67cc087866c64e85453639
Ibrahem ahmad <53955363+ibrahemahmad@users.noreply.github.com>
2022-09-20 11:21:34 +0300

    add one more slice for todos

---
+++ src/redux/reducers/todoSlice.js
@@ -0,0 +1,69 @@
+import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit'
+import { act } from 'react-dom/test-utils'
+import { constats } from '../../config/constant'
+import { axiosGet } from '../../config/globalAxios'
+
+export const fetchTodos = createAsyncThunk('todos/fetchTodos', async (type) => {
+console.log("check type",type);
+  try {
+    const response = await axiosGet({
+      url: `https://jsonplaceholder.typicode.com/${type}`
+    })
+    return [...response]
+  } catch (error) {
+    return error.message
+  }
+})
+export const todoSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    todos: [],
+    counts: [],
+    status: constats.idle,
+    error: ''
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementCount: {
+      //ONLY FOR TEST :// this way for change anything in payload before adding into state.
+      //for call dispatch(incrementCount(1))
+
+      reducer(state, action) {
+        state.counts.push(action.payload)
+      },
+      prepare(number) {
+        return {
+          payload: {
+            number: number,
+            id: nanoid()
+          }
+        }
+      }
+    }
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchTodos.pending, (state, action) => {
+      state.status = constats.loading
+    })
+    builder.addCase(fetchTodos.rejected, (state, action) => {
+      state.status = constats.rejected
+      state.error = action.error.message
+    })
+    builder.addCase(fetchTodos.fulfilled, (state, action) => {
+      state.status = constats.success
+      state.todos = state.todos.concat(action.payload)
+    })
+  }
+})
+export const getAllTodos = (state) => state.todos.todos
+export const getTodoStatus = (state) => state.todos.status
+export const getTodoError = (state) => state.todos.error
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementCount } = todoSlice.actions
+
+export default todoSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import countSlice  from './reducers/countSlice'
+import todoSlice from './reducers/todoSlice'
 import useReducer from './reducers/userReducer'
 
 export default  configureStore({
    reducer:{
     user:useReducer,
     count:countSlice,
+    todos:todoSlice
    }
 })
 	
