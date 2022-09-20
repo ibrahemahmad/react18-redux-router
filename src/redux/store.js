86867f091dfe8a695a7b4b74193e173661ad9ac4
Ibrahem ahmad <53955363+ibrahemahmad@users.noreply.github.com>
2022-09-20 13:40:05 +0300

    add meddilware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
-import countSlice  from './reducers/countSlice'
+import countSlice from './reducers/countSlice'
+import { todosApi } from './reducers/todoRtkSlice'
 import todoSlice from './reducers/todoSlice'
 import useReducer from './reducers/userReducer'
-
-export default  configureStore({
-   reducer:{
-    user:useReducer,
-    count:countSlice,
-    todos:todoSlice
-   }
+import { setupListeners } from '@reduxjs/toolkit/query'
+const store = configureStore({
+  reducer: {
+    user: useReducer,
+    count: countSlice,
+    todos: todoSlice,
+    [todosApi.reducerPath]: todosApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([todosApi.middleware])
 })
-	
+export default store
+
+setupListeners(store.dispatch)
